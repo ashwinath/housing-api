14ee50677dcab21955ef774c97300860e4df1e6b
Ashwin Chatterji <ashwinath@hotmail.com>
2022-09-06 11:44:33 +0000

    Cast env var to number

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from typing import Any, Dict, Generator, Iterable, Union
 RESOURCE_ID_HDB_RESALE = "f1765b54-a209-4718-8d38-a39237f502b3"
 cache = {} # cache_key: {data: {}, cache_expire: date}
 URL_STRING = "https://data.gov.sg/api/action/datastore_search"
-BATCH_SIZE = os.environ.get("BATCH_SIZE", 5)
+BATCH_SIZE = int(os.environ.get("BATCH_SIZE", 5))
 
 class HealthCheckHandler(tornado.web.RequestHandler):
     async def get(self):
